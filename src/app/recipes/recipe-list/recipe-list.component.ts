62c071c8b8477b913b71af4c761cf7b79a999f74
marialapc <marialopez.apc@gmail.com>
2023-11-25 14:09:19 +0100

    Using a service for cross-component comunication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output,  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -8,7 +8,6 @@ import { RecipeService } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes!: Recipe[];
 
  constructor (private recipeService: RecipeService) {
@@ -17,8 +16,4 @@ export class RecipeListComponent {
 ngOnInit() {
   this.recipes= this.recipeService.getRecipes();
 }
-
- onRecipeSelected(recipe: Recipe) {
-  this.recipeWasSelected.emit(recipe);
- }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model'
 
 export class RecipeService {
+recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
         new Recipe('A test Recipe','This is simply a test','https://www.annarecetasfaciles.com/files/miniatura-6-1024x575.jpg'),
         new Recipe('Another test Recipe','This is simply a test','https://www.annarecetasfaciles.com/files/miniatura-6-1024x575.jpg')
