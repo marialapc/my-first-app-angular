c70123c450a3303755e461f55529f186b6ad0ce7
marialapc <marialopez.apc@gmail.com>
2023-11-26 10:32:50 +0100

    add ingredients to the list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model'
+import { Ingredient } from '../shared/ingredients.model';
 
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-        new Recipe('A test Recipe','This is simply a test','https://www.annarecetasfaciles.com/files/miniatura-6-1024x575.jpg'),
-        new Recipe('Another test Recipe','This is simply a test','https://www.annarecetasfaciles.com/files/miniatura-6-1024x575.jpg')
+        new Recipe(
+            'Tasty Schnitzel',
+            'This is simply a test',
+            'https://www.annarecetasfaciles.com/files/miniatura-6-1024x575.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Big fat burger',
+            'what do yo need?',
+            'https://www.annarecetasfaciles.com/files/miniatura-6-1024x575.jpg',
+             [
+            new Ingredient('Buns', 2),
+            new Ingredient('Meat', 1)
+        ])
       ];
 
    getRecipes() {
