35edf743b75f9cd21d3a1096f3725f8223d2c2ee
marialapc <marialopez.apc@gmail.com>
2024-01-17 19:51:21 +0100

    Change the subscription Name

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -12,13 +12,13 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients!: Ingredient[];
-  private igChangeSub!: Subscription;
+  private subscription!: Subscription;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-     this.igChangeSub = this.slService.ingredientsChanged
+     this.subscription = this.slService.ingredientsChanged
     .subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
@@ -27,6 +27,6 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.igChangeSub.unsubscribe();
+    this.subscription.unsubscribe();
   }
 }
